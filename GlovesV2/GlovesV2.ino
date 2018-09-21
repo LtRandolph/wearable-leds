63fd1f2f2627ff7de91aff59dfdcde70dfcd8c4a
Unknown <shaidarelam@gmail.com>
2018-09-20 18:52:14 -0700

    Major work on gloves to prepare for tutorializing

--- GlovesV2/GlovesV2.ino
+++ GlovesV2/GlovesV2.ino
@@ -1,33 +1,49 @@
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
+// Library for accelerometer chip.
 #include <Adafruit_LSM303_U.h>
+
+// Library for keeping state upon reboot.
 #include <EEPROM.h>
 
+// Library for writing to LEDs.
+#include <Adafruit_NeoPixel.h>
+
+// Libraries to allow setting "low-power mode".
 #include <avr/sleep.h>
 #include <avr/interrupt.h>
-
-#include <Adafruit_NeoPixel.h>
 
 // Acceleration for dance mode. Magnet for compass mode. Each need a unique identifier.
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
 Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
 
+// The control pin of the Neopixel ring.
 #define PIN 0
+// The number of LEDs on the Neopixel ring.
 #define NUM_LEDS 16
 
-enum class Mode
+// Modes that will be cycled between every time power is reset.
+enum Mode
 {
-  Dance = 0,
-  Compass = 1,
-  Off = 2,
-  Count
+  kModeDance = 0,
+  kModeCompass = 1,
+  kModeOff = 2,
+  kModeCount // convenience value to allow easy modulo
 };
 
+// The actual LED structure.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, PIN);
 
-Mode mode = Mode::Off;
+// Declare some global variables for control.
+Mode mode = kModeOff;
 bool isLeftGlove = false;
 
+// This mode makes the Arduino drain less power when the lEDs are off, but can make the Arduino
+// unresponsive to new uploads. So only turn this on when you're heading out into the desert.
+// To recover from this state, get the gloves into a state where neither the current state
+// or the next state are low-power, so the IDE can do one cycle and have the Arduino stay on.
+bool enableLowPowerOffMode = false;
+
+// Based on which glove we're in, we'll compute these useful angle values once at startup.
+float compassTheta;
 float sinT;
 float cosT;
 
@@ -36,26 +52,40 @@ void setup()
   // Helpful for debugging. Turn off in production.
   Serial.begin(9600);
 
+  // Turn off the on-board LED.
   digitalWrite(7, LOW);
 
   // Run this line once on only the left glove to permanently mark it as the left one.
   //EEPROM.write(1, 1);
+  // Run this line once on only the right glove to permanently mark it as the right one.
+  //EEPROM.write(1, 0);
 
+  // Based on having run the above line once (or more) on the left glove, we can read the value back
+  // to see if we're on the left or right glove.
   isLeftGlove = EEPROM.read(1) != 0;
-  
-  const float theta = (isLeftGlove ? 30 : 50) * (PI / 180);
-  sinT = sin(theta);
-  cosT = cos(theta);
+
+  // You'll need to adjust these values by hand to get your compass readings correct, and the behavior
+  // of colors in dance mode to match. It will vary based on the geometry of how you sewed the gloves.
+  // I tried really hard to find a "unifying theta" but the tunings didn't line up.
+  compassTheta = (isLeftGlove ? 145 : 170) / 360.0;
+  const float danceTheta = isLeftGlove ? 25 : 80;
+  sinT = sin(danceTheta * (PI / 180));
+  cosT = cos(danceTheta * (PI / 180));
 
   // Every time you power cycle (by disconnecting or hitting reset), cycle to the next mode.
-  mode = (Mode)EEPROM.read(0);
-  EEPROM.write(0, ((int)mode + 1) % (int)Mode::Count);
+  mode = EEPROM.read(0);
+  EEPROM.write(0, (mode + 1) % kModeCount);
+
+  // Debug mode to let you focus on one mode at a time.
+  // mode = kModeCompass;
   
   pixels.begin();
   accel.begin();
   mag.begin();
 
-  if (mode == Mode::Off)
+  // If we're off, make sure the LEDs are turned off, then put the arduino to sleep for power saving.
+  // Note that this may make the Arduino temporarily not respond to uploads from the IDE.
+  if (mode == kModeOff && enableLowPowerOffMode)
   {
     pixels.show();
     noInterrupts();
@@ -65,6 +95,8 @@ void setup()
   }
 }
 
+// Struct to do damped motion towards a target value between 0 and 1. The damping is pretty simplistic
+// but it seems to get the job done.
 struct ClampValue
 {
   float cur = 0;
@@ -76,6 +108,7 @@ struct ClampValue
     vel += (targetVel - vel) * dt;
     cur += vel * dt;
 
+    // Don't shoot past our target value; just stop at it.
     if (vel > 0 && cur > targetVal)
     {
       cur = targetVal;
@@ -87,20 +120,23 @@ struct ClampValue
       vel = 0;
     }
 
+    // Make sure to stay clamped.
     if (cur < 0) cur = 0;
     if (cur > 1) cur = 1;
   }
 };
 
+// Struct to "rotate" around a circle that goes from 0 to 1 and wraps. Wants a specified velocity, since there are no "special"
+// values on a wrapped ring.
 struct WrapValue
 {
   float cur = 0;
-  float vel = 0;
 
-  void Tick(float dt, float targetVel)
+  void Tick(float dt, float vel)
   {
-    vel += (targetVel - vel) * dt;
     cur += vel * dt;
+
+    // Wrap to stay between 0 and 1.
     while (cur < 0) cur += 1;
     while (cur > 1) cur -= 1;
   }
@@ -122,35 +158,44 @@ float zPrev = 0;
 
 void LoopDance()
 {
+  // Get our current acceleration.
   sensors_event_t event;
   accel.getEvent(&event);
 
+  // X and Y need to be rotated based on the way we sewed the glove together if we want holding the glove
+  // vertically to mean the same thing for both hands. I like to tune it so vertical is green and horizontal
+  // is red.
   const float xMag = min(1, abs(event.acceleration.x * cosT - event.acceleration.y * sinT) / 10);
   const float yMag = min(1, abs(event.acceleration.y * cosT + event.acceleration.x * sinT) / 10);
   const float zMag = min(1, abs(event.acceleration.z) / 10);
 
+  // Now compute the values we need to see how much acceleration has changed since last tick (aka "jerk").
   const float xDiff = xMag - xPrev;
   const float yDiff = yMag - yPrev;
   const float zDiff = zMag - zPrev;
+  const float jerk = sqrt(xDiff * xDiff + yDiff * yDiff + zDiff * zDiff);
+
+  // Save off this tick's values.
   xPrev = xMag;
   yPrev = yMag;
   zPrev = zMag;
-  const float jerk = sqrt(xDiff * xDiff + yDiff * yDiff + zDiff * zDiff);
 
-  Serial.print("Jerk: "); Serial.println(jerk);
+  const float dt = 0.25;
 
-  const float totalMag = sqrt(xMag * xMag + yMag * yMag + zMag * zMag);
-
-  const float dt = totalMag * 0.25;
-
+  // Aim the R/G/B at the magnitude of acceleration in that direction (roughly according to orientation to gravity).
   r.Tick(dt, xMag);
   g.Tick(dt, yMag);
   b.Tick(dt, zMag);
 
-  brightness.Tick(0.25, jerk * jerk * 2);
+  // Aim the brightness at a value corresponding to jerk. Squaring it reduces how flickery it is at low values, since
+  // it mostly just stays towards 0 unless you have high jerk. Times 2 just due to me messing around with it and liking
+  // the feel of that number.
+  brightness.Tick(dt, jerk * jerk * 2);
 
+  // Make sure the pixels always stay on, but will be bright only if jerk is high.
   pixels.setBrightness(max(5, 255 * brightness.cur));
 
+  // Rotate the pixels around the ring.
   rot.Tick(dt, 0.25);
 
   // Divide the ring into a certain number of divs, with "before/after" bonus divisions to help with wrapping.
@@ -167,11 +212,36 @@ void LoopDance()
     {
       dim = min(dim, pow(0.5 * abs(rotDivs[j] - i), 2));
     }
-    //dim = max(dim, 0.1);
     pixels.setPixelColor(i, dim * r.cur * 255, dim * g.cur * 255, dim * b.cur * 255);
   }
 }
 
+// Ok this part sucks. Your Arduino and LEDs will have their own generated magnetic field that you need to
+// subtract out. Run with FindCompassCalibration on and the glove far away from any other electronics.
+// Then, rotate it as much as possible with the Serial monitor open, until you no longer see the max/min
+// values changing. Then set the appropriate calibration values below for the glove to the 3 values printed
+// in the second line. For verification, I typically see a range of about 90 micro-Teslas in X/Y axes.
+float maxX = -1000; float minX = 1000;
+float maxY = -1000; float minY = 1000;
+float maxZ = -1000; float minZ = 1000;
+void FindCompassCalibration(float x, float y, float z)
+{
+  maxX = max(maxX, x); minX = min(minX, x);
+  maxY = max(maxY, y); minY = min(minY, y);
+  maxZ = max(maxZ, z); minZ = min(minZ, z);
+
+  Serial.print(minX); Serial.print(" "); Serial.print(maxX); Serial.print(" ");
+  Serial.print(minY); Serial.print(" "); Serial.print(maxY); Serial.print(" ");
+  Serial.print(minX); Serial.print(" "); Serial.println(maxZ);
+
+  Serial.print(-(maxX + minX) / 2); Serial.print(" "); Serial.print(-(maxY + minY) / 2); Serial.print(" "); Serial.println(-(maxZ + minZ) / 2);
+}
+
+// These are my magnetic calibration values, as described above. You need to change these for an accurate compass.
+float calibrationLeft[3] = { 4.77, 5.05, 61.43 };
+float calibrationRight[3] = { 13.41, -12.09, 0.92 };
+
+// Simple helper function to reduce copy paste below.
 float GetAlphaFromDistance(float valA, float valB, float radius)
 {
   return max(0, (radius - abs(valA - valB)) / radius);
@@ -182,33 +252,48 @@ void LoopCompass()
   sensors_event_t event;
   mag.getEvent(&event);
 
+  // When you're calibrating your compass, comment this line out and follow the instructions above.
+  // FindCompassCalibration(event.magnetic.x, event.magnetic.y, event.magnetic.z);
+
+  float* calibration = isLeftGlove ? calibrationLeft : calibrationRight;
+
+  event.magnetic.x += calibration[0];
+  event.magnetic.y += calibration[1];
+  event.magnetic.z += calibration[2];
+
   float angle = atan2(event.magnetic.x, event.magnetic.y) + PI;
   angle /= (2 * PI); // normalize to (0,1) range
-  // In my tests, I'm seeing it rotating the wrong way, so flip it.
+  
+  // In my tests, I'm seeing it rotating the wrong way, so flip it
   angle = 1.0f - angle;
 
-  angle += isLeftGlove ? 0.5f : 0.0f;
-  if (angle > 1.0f) angle -= 1.0f;
-  
+  angle += compassTheta; // rotate by the appropriate amount for which glove we are
+  if (angle > 1.0f) angle -= 1.0f; // loop back to a range from 0 to 1
+  if (angle < 0.0f) angle += 1.0f; // loop back to a range from 0 to 1
+
+  // We're going to show a marker where north is, which will fall off in brightness over the given radius
   const float markerCenter = angle * NUM_LEDS;
   const float markerRadius = 1.5f;
 
   for (int i = 0; i < NUM_LEDS; ++i)
   {
-    // Watch for wrapping at the top of the range by subtracting NUM_LEDS from the center.
+    // Watch for wrapping at the top of the range by subtracting NUM_LEDS from the center
     float dim = max(GetAlphaFromDistance(i, markerCenter, markerRadius), GetAlphaFromDistance(i, markerCenter- NUM_LEDS, markerRadius));
 
-    pixels.setPixelColor(i, dim * 70, dim * 70, dim * 120);
+    // I like a dim red in case I'm preserving night vision.
+    const byte color[3] = { 0x33, 0x00, 0x00 };
+
+    pixels.setPixelColor(i, dim * color[0], dim * color[1], dim * color[2]);
   }
 }
 
 void loop()
 {
-  if (mode == Mode::Dance)
+  if (mode == kModeDance)
   {
     LoopDance();
   }
-  else if (mode == Mode::Compass)
+  else if (mode == kModeCompass)
   {
     LoopCompass();
   }
